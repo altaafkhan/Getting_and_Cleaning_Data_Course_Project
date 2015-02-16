bdd387fe190f003643720191e0583252723d7d39
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-02-16 15:30:05 +1100

    Create run_analysis.R
    created run_analysis.R and included comments around the data cleansing steps

---
+++ run_analysis.R
@@ -0,0 +1,17 @@
+## Script for course project associated with Getting and Cleaning Data offered by John Hopkins Bloomberg School of Public Health
+
+
+## 1. Merges the training and the test sets to create one data set.
+
+
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+
+## 3. Uses descriptive activity names to name the activities in the data set
+
+
+## 4. Appropriately labels the data set with descriptive variable names. 
+
+
+## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
